083c8d64ec59cc536af9ba9ca83b42cbc2b00d43
lafmaer <98372973+lafmaer@users.noreply.github.com>
2024-01-30 15:21:27 +0800

    change

---
+++ src/date.ac1208a7.js
@@ -0,0 +1,1 @@
+const e=new Date;let t=e.getFullYear();e.toLocaleDateString();const a=()=>({year:t});export{a as g};
---
+++ src/default.15c2d9c7.js
@@ -0,0 +1,1 @@
+import{_ as r,r as t}from"./entry.00688b6a.js";const s={};function n(e,o){return t(e.$slots,"default")}const a=r(s,[["render",n]]);export{a as default};
